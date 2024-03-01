85074acfd229e6bfdf43dab23f6e152c9f434b47
Mr. YEZER <02230089.cst@rub.gov.bt>
2024-03-01 16:24:14 +0600

    commit code

---
+++ cw.py
@@ -0,0 +1,9 @@
+Age=int(input("Enter your age: \n"))
+yn=input("Are you student? (yes/no): \n")
+
+if Age<12 or Age==12:
+    print("You are eligible for discount on the movie ticket")
+elif Age>=13 and Age<18 and yn=="yes":
+    print("You are  eligible for the discount")
+else:
+    print("you are not eligible")
