8a5a3fb19c5511627a6486fe240eb2f9d1fcbc1d
Mr. YEZER <02230089.cst@rub.gov.bt>
2024-03-06 14:11:33 +0600

    commit code

--- prompt.py
+++ prompt.py
@@ -7,5 +7,6 @@ add_result = num1+num2
 substract_result = num1-num2
 multiply_result = num1*num2
 division_result = num1/num2
+
 print(f"Hello!{name} the sum of two number is: \n {add_result} \n  and the difference between the two number is:  \n {division_result}.\n")
 d=input("my dog name is")
