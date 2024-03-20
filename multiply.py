3cd4f92b90efa81c7e31d72840ff3b3b253b543c
Mr. YEZER <02230089.cst@rub.gov.bt>
2024-03-20 15:03:11 +0600

    commit code

--- multiply.py
+++ multiply.py
@@ -1,4 +1,6 @@
 x=int(input("Enter the number for which you want to multiple: "))
 y=int(input("Enterthe limit upto which you want the multiplication table generated: "))
-for i in range(1,y+1):
-    
+for i in range(x,x+1):
+    print(i)
+    for j in range(y):
+        print(f"{i}x{j}={i*j}")
